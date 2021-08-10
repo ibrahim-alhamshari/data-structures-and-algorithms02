52e73b8bd9ab4a0f5d7d098ca4fb0241732ef460
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-08-10 15:33:25 +0300

    alot of changes to my challenge12

---
+++ codeChallenge12/src/main/java/codeChallenge12/CatClass.java
@@ -0,0 +1,18 @@
+package codeChallenge12;
+
+public class CatClass {
+
+    private String cat;
+
+    public CatClass(String cat){
+        this.cat=cat;
+    }
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+}
